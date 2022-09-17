d98b7648fdf7a63fe3a3578c7eac06b4305f3151
Thaís A <thais.antero@gmail.com>
2022-09-17 12:12:49 -0300

    Usuário vê detalhes do galpão e volta pra tela inicial

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -8,7 +8,7 @@ describe 'Usuário ve detalhes de um galpao' do
                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
                     description: 'Galpão destinado para cargas internacionais')
     # Act
-    visit('/')
+    visit(root_path)
     click_on('Aeroporto de SP')
 
     # Assert
@@ -18,6 +18,19 @@ describe 'Usuário ve detalhes de um galpao' do
     expect(page).to have_content('Área: 100000 m2')
     expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
     expect(page).to have_content('Galpão destinado para cargas internacionais')
+  end
 
+  it 'e volta para a tela inicial' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto de SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+          address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+          description: 'Galpão destinado para cargas internacionais')
+    # Act
+    visit(root_path)
+    click_on('Aeroporto de SP')
+    click_on('Voltar')
+
+    # Assert
+    expect(current_path).to have_content(root_path)
   end
 end 
