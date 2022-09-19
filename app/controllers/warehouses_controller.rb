351ab38dad8adb51383c3bd0ec41549afbad8e80
Thaís A <thais.antero@gmail.com>
2022-09-19 11:23:27 -0300

    Implementa formulário para preenchimento das informações de novo galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,18 @@ class WarehousesController < ApplicationController
     @warehouse = Warehouse.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+
+
+    warehouse_params = params.require(:warehouse).permit(:name, :city,
+    :area, :code, :address, :cep, :description)
+    w = Warehouse.new(warehouse_params)
+    w.save
+
+    redirect_to root_path
+  end
 end
