71b470ac309e7f3dc2e55e956427722ad082849e
Thaís A <thais.antero@gmail.com>
2022-09-20 09:30:53 -0300

    Inclui mensagem de sucesso ao cadastrar galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -8,13 +8,12 @@ class WarehousesController < ApplicationController
   end
 
   def create
-
-
     warehouse_params = params.require(:warehouse).permit(:name, :city,
     :area, :code, :address, :cep, :description)
     w = Warehouse.new(warehouse_params)
     w.save
-
+    
+    flash[:notice] = 'Galpão cadastrado com sucesso.'
     redirect_to root_path
   end
 end
--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -36,6 +36,7 @@ describe 'Usuário cadastra galpão' do
 
     # Assert
     expect(current_path).to eq root_path
+    expect(page).to have_content('Galpão cadastrado com sucesso.')
     expect(page).to have_content('Rio de Janeiro')
     expect(page).to have_content('RIO')
     expect(page).to have_content('32000 m2')
