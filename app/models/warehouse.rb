1317b27352bf71d775dccb7910b511e2382e5903
Thaís A <thais.antero@gmail.com>
2022-09-21 09:51:27 -0300

    Implementa validação da presença dos dados e singularidade do código

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,5 @@
 class Warehouse < ApplicationRecord
   validates :name, :city, :area, :code, :address, :cep, :description, presence: true
   validates :code, length: { is: 3 }
+  validates :code, uniqueness: true
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe 'válido?' do
+    context 'presença' do
+      it 'falso quando nome está vazio' do
+        warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço',
+                                  cep: '25000-000', city: 'Rio', area: 1000,
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando nome código está vazio' do
+        warehouse = Warehouse.new(name: 'Rio', code: '', address: 'Endereço',
+                                  cep: '25000-000', city: 'Rio', area: 1000,
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando endereço está vazio' do
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: '',
+                                  cep: '25000-000', city: 'Rio', area: 1000,
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando CEP está vazio' do
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço',
+                                  cep: '', city: 'Rio', area: 1000,
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando cidade está vazio' do
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço',
+                                  cep: '25000-000', city: '', area: 1000,
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando área está vazia' do
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço',
+                                  cep: '25000-000', city: 'Rio', area: '',
+                                  description: 'Alguma descrição')
+
+        expect(warehouse).not_to be_valid
+      end
+
+      it 'falso quando descrição está vazia' do
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Endereço',
+                                  cep: '25000-000', city: 'Rio', area: 1000,
+                                  description: '')
+
+        expect(warehouse).not_to be_valid
+      end
+    end
+
+    context 'singularidade dos atributos' do
+      it 'falso quando código já foi utilizado' do
+        warehouse_first = Warehouse.create(name: 'Rio', code: 'RIO', address: 'Endereço',
+                                          cep: '25000-000', city: 'Rio', area: 1000,
+                                          description: 'Galpão no Rio')
+        warehouse_second = Warehouse.new(name: 'Niteroi', code: 'RIO', address: 'Av Niteroi',
+                                        cep: '35000-000', city: 'Niteroi', area: 2000,
+                                        description: 'Galpão em Niteroi')
+
+        expect(warehouse_second).not_to be_valid
+      end
+    end
+  end
 end
