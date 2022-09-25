00af88051edb5a95cc93a47e074864687f115345
Thaís A <thais.antero@gmail.com>
2022-09-25 10:46:55 -0300

    Implementa funcionalidade para ver nome e cidade dos fornecedores a partir do menu principal
    Arquivos modificados para tal funcionadalidade:
    
    * Arquivo view index.html.erb
    * Arquivo de teste da apresentação do nome e cidade da empresa

--- spec/system/supplier/user_view_suppliers_spec.rb
+++ spec/system/supplier/user_view_suppliers_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe 'Usuário vê fornecedores' do
   it 'a partir do menu' do
-
     visit root_path
     click_on 'Fornecedores'
 
     expect(current_path).to eq suppliers_path
   end
 
-  
+  it 'com sucesso' do
+    Supplier.create!(corporate_name: 'ACME LTDA', brand_name:'ACME', registration_number:'123435678', 
+                      full_address:'Av das Palmas, 100', city:'Bauru - SP', state:'SP', email:'contato@gmail.com')
+    Supplier.create!(corporate_name: 'Spark Industries Brasil', brand_name:'Spark', registration_number:'34567891', 
+                      full_address:'Terra da Indústria, 12', city:'Teresina', state:'PI', email:'contato_spark@gmail.com')
+
+    visit root_path
+    click_on 'Fornecedores'
+
+    expect(page).to have_content 'ACME'
+    expect(page).to have_content 'Bauru - SP'
+    expect(page).to have_content 'Spark'
+    expect(page).to have_content 'Teresina'
+  end
 end
