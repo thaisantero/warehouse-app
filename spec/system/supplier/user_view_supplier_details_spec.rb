c24e23f6252130f3580dfa04ce56d2a2c23ad946
Thaís A <thais.antero@gmail.com>
2022-09-25 12:08:43 -0300

    Implementa funcionalidade para voltar para o menu inicial estando na páágina de detalhes do fornecedor
    Para isso foram modificados os seguintes arquivos:
    
    * Arquivo view show.html.erb, no qual foi incluído link
    para retornar a root_path
    * Arquivo de teste user_view_supplier_details_spec.rb, o qual checa
    a funcionalidade de voltar para o menu inicial estando na página de
    detalhes do fornecedor

--- spec/system/supplier/user_view_supplier_details_spec.rb
+++ spec/system/supplier/user_view_supplier_details_spec.rb
@@ -18,4 +18,20 @@ describe 'Usuário vê detalhes de um fornecedor' do
     expect(page).to have_content('Endereço: Av das Palmas, 100 - Bauru - SP')
     expect(page).to have_content('E-mail: contato@gmail.com')
   end
+
+  it 'e volta para a tela inicial' do
+    # Arrange
+    Supplier.create!(
+      corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '123435678',
+      full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@gmail.com'
+    )
+    # Act
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'ACME'
+    click_on 'Voltar'
+
+    # Assert
+    expect(current_path).to have_content(root_path)
+  end
 end
