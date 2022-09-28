de97f8bc6157b0f075d6b95879e019e9859a3dae
Thaís A <thais.antero@gmail.com>
2022-09-28 05:36:31 -0300

    Implementa funcionaidade para usuário criar conta.
    Para realizar tal funcionalidade foi necessário alterar as telas
    de sessão e de registro, criar arquivo 18i-n, incluir nos strong
    parameters do device o atributo name do model user e realizar o
    teste para certificar que o usuário está fazendo o cadastro com sucesso
    e que seus atributos (email, senha e nome) estão sendo salvos.

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -14,8 +14,28 @@ describe 'Usuário se autentica' do
     expect(page).to have_content 'Login efetuado com sucesso.'
     within('nav') do
       expect(page).not_to have_link 'Entrar'
-      expect(page).to have_link 'Sair'
+      expect(page).to have_button 'Sair'
       expect(page).to have_content 'joao@gmail.com'
+    end
+  end
+
+  it 'e faz logout' do
+    User.create!(email: 'joao@gmail.com', password: 'password')
+
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'joao@gmail.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    within('nav') do
+      expect(page).to have_link 'Entrar'
+      expect(page).not_to have_button 'Sair'
+      expect(page).not_to have_content 'joao@gmail.com'
     end
   end
 end
